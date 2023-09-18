a9d4963b327fdf5eedae746a3ab7d7bc76865fb9
MurrayIII <murrays3@live.com>
2023-09-18 10:06:54 -0700

    For some reason, ummlConfig.forceMathJax is inverted

--- playground/assets/playground.js
+++ playground/assets/playground.js
@@ -830,7 +830,8 @@ async function draw() {
     output_mathml_ast.innerHTML = output_mathml_ast_HTML;
     output_source.innerHTML = output_source_HTML;
 
-    if (ummlConfig.forceMathJax) {
+    if (!ummlConfig.forceMathJax) {
+        // TODO: why is forceMathJax inverted?
         output.attachShadow({ mode: 'open' });
         output.shadowRoot.innerHTML = output_HTML;
         MathJax.typeset([output]);
